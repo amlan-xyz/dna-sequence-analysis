ce0452cec0c5565f84c0284fd959629f6fcca67d
Amlanjyoti Saikia <heyamlan19@gmail.com>
2022-11-22 19:56:49 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,28 @@
+from Bio.Seq import Seq
+from Bio import Entrez, SeqIO
+Entrez.email="amlansaikia1918@gmail.com"
+handle = Entrez.efetch(db="nucleotide", id="AY851612", rettype="gb", retmode="text")
+recs=list(SeqIO.parse(handle,'gb'))
+handle.close()
+print(recs)
+covid_dna=recs[0].seq
+# print(covid_dna)
+print(f'The genome of Covid -19 consist of {len(covid_dna)} nucleotides')
+
+from Bio.SeqUtils import gc_fraction
+print(gc_fraction(covid_dna))
+
+count_nucleotides={
+	'A': covid_dna.count('A'),
+ 	'T': covid_dna.count('T'),
+	'C': covid_dna.count('C'),
+	'G': covid_dna.count('G'),
+}
+print(count_nucleotides)
+
+import matplotlib.pyplot as plt
+width=0.5
+print(plt.bar(count_nucleotides.keys(),count_nucleotides.values(),width,color=['b','r','m','c']))
+print(plt.xlabel('Nucleotide'))
+print(plt.ylabel('Frequency'))
+print(plt.title('Nucleotide Frequency'))
