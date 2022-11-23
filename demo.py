8d4902e40ca0991a4d18562d3f32a05d80b7006a
Amlanjyoti Saikia <heyamlan19@gmail.com>
2022-11-23 23:59:38 +0530

    removed comment

--- demo.py
+++ demo.py
@@ -9,23 +9,23 @@ covid_dna=recs[0].seq
 # print(covid_dna)
 print(f'The genome of Covid -19 consist of {len(covid_dna)} nucleotides')
 
-# from Bio.SeqUtils import gc_fraction
-# print(gc_fraction(covid_dna))
+from Bio.SeqUtils import gc_fraction
+print(gc_fraction(covid_dna))
 
-# count_nucleotides={
-# 	'A': covid_dna.count('A'),
-#  	'T': covid_dna.count('T'),
-# 	'C': covid_dna.count('C'),
-# 	'G': covid_dna.count('G'),
-# }
-# print(count_nucleotides)
+count_nucleotides={
+	'A': covid_dna.count('A'),
+ 	'T': covid_dna.count('T'),
+	'C': covid_dna.count('C'),
+	'G': covid_dna.count('G'),
+}
+print(count_nucleotides)
 
 import matplotlib.pyplot as plt
-# width=0.5
-# print(plt.bar(count_nucleotides.keys(),count_nucleotides.values(),width,color=['b','r','m','c']))
-# print(plt.xlabel('Nucleotide'))
-# print(plt.ylabel('Frequency'))
-# print(plt.title('Nucleotide Frequency'))
+width=0.5
+print(plt.bar(count_nucleotides.keys(),count_nucleotides.values(),width,color=['b','r','m','c']))
+print(plt.xlabel('Nucleotide'))
+print(plt.ylabel('Frequency'))
+print(plt.title('Nucleotide Frequency'))
 
 # transcription
 covid_mrna=covid_dna.transcribe()
@@ -37,8 +37,8 @@ covid_aa=covid_mrna.translate()
 print(covid_aa)
 
 #most common amino acids
-print("##########most common amino acids############")
-
+print("########## most common amino acids ############")
+ 
 from collections import Counter
 common_amino=Counter(covid_aa)
 common_amino.most_common(10)
@@ -46,7 +46,7 @@ common_amino.most_common(10)
 del common_amino['*']
 
 width=0.5
-print(plt.bar(common_amino.keys(),common_amino.values(),width,colors=['b','r','m','c']))
+print(plt.bar(common_amino.keys(),common_amino.values(),width,color=['b','r','m','c']))
 print(plt.xlabel('Amino Acids'))
 print(plt.ylabel('Frequency'))
 print(plt.title('Protein Sequence Frequency'))
